9564575f80baf61829aa16d4b909e5bcb30376d5
Berhe <demars.berhe@icfnext.com>
2022-07-19 14:25:30 -0400

    Remove assets/uswds/ from .gitignore and add copyAll command to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ const uswds = require("@uswds/compile");
 uswds.paths.dist.css = './assets/uswds/dist/css';
 uswds.paths.dist.fonts = './assets/uswds/dist/fonts';
 uswds.paths.dist.theme = './assets/uswds/dist/theme';
-uswds.paths.dist.img - './assets/uswds/dist/img';
+uswds.paths.dist.img = './assets/uswds/dist/img';
 uswds.paths.dist.js = './assets/uswds/dist/js';
 
 /**
@@ -23,4 +23,5 @@ uswds.paths.dist.js = './assets/uswds/dist/js';
  * Add as many as you need
  */
  exports.init = uswds.init; //initialize design system on the project; command: npx gulp init
+ exports.copyAll = uswds.copyAll;
  exports.compile = uswds.compile; // compile sass files; command: npx gulp compile
